d82e85bfdb7bf9426e6643f4c41341ee7550b19e
Pablo <ezekielgonzalezr@gmail.com>
2023-05-18 16:49:33 -0600

    moved functions to modules

--- src/index.js
+++ src/index.js
@@ -1,57 +1,17 @@
 import './style.css';
+import getScores from './modules/getScores.js'
+import addScore from './modules/addScore.js';
+import displayMessage from './modules/displayMessage.js';
 
 const refresh = document.querySelector('#refresh');
-const listContainer = document.querySelector('#list-container');
+const submit = document.querySelector('#submit');
+const name = document.querySelector('#name');
+const score = document.querySelector('#score');
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Iv4RO4M90HFElzEus2q3/scores/';
-
-const getScores = async (file) => {
-  const resp = await fetch(file);
-  const response = await resp.json();
-  listContainer.innerHTML = '';
-  response.result.forEach((ele) => {
-    const list = document.createElement('li');
-    list.innerHTML = `<spam>${ele.user}</spam><spam>${ele.score}</spam>`;
-    listContainer.appendChild(list);
-  });
-};
 
 document.addEventListener('DOMContentLoaded', () => getScores(url));
 
 refresh.addEventListener('click', () => getScores(url));
-
-const submit = document.querySelector('#submit');
-
-const addScore = async (file, data) => {
-  const responses = await fetch(file, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  return responses.json();
-};
-const name = document.querySelector('#name');
-const score = document.querySelector('#score');
-const messageC = document.querySelector('#message-container');
-
-const displayMessage = (mes) => {
-  messageC.innerHTML = '';
-  const text = document.createElement('p');
-  if (mes.result) {
-    text.innerHTML = mes.result;
-    text.classList.add('success');
-    name.value = '';
-    score.value = '';
-  } else {
-    text.innerHTML = mes.message;
-    text.classList.add('error');
-  }
-  messageC.appendChild(text);
-  setTimeout(() => {
-    messageC.innerHTML = '';
-  }, 3000);
-};
 
 submit.addEventListener('click', (e) => {
   e.preventDefault();
@@ -62,3 +22,5 @@ submit.addEventListener('click', (e) => {
     displayMessage(data);
   });
 });
+
+export {name, score};
