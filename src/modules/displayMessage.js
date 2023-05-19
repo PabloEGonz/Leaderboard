708ab6d76da8d1276d93d09c32dd9e76f1514769
Pablo <ezekielgonzalezr@gmail.com>
2023-05-19 16:50:36 -0600

    created modules files

---
+++ src/modules/displayMessage.js
@@ -0,0 +1,21 @@
+const messageC = document.querySelector('#message-container');
+import { name, score } from "../index.js";
+const displayMessage = (mes) => {
+    messageC.innerHTML = '';
+    const text = document.createElement('p');
+    if (mes.result) {
+      text.innerHTML = mes.result;
+      text.classList.add('success');
+      name.value = '';
+      score.value = '';
+    } else {
+      text.innerHTML = mes.message;
+      text.classList.add('error');
+    }
+    messageC.appendChild(text);
+    setTimeout(() => {
+      messageC.innerHTML = '';
+    }, 3000);
+  };
+
+export default displayMessage;
