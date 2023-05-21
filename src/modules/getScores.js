4617ea13960992243ac2f6553473f95491e2c37d
Pablo <ezekielgonzalezr@gmail.com>
2023-05-20 18:51:15 -0600

    sorted by highest to lowest scores

--- src/modules/getScores.js
+++ src/modules/getScores.js
@@ -4,6 +4,13 @@ const getScores = async (file) => {
   const resp = await fetch(file);
   const response = await resp.json();
   listContainer.innerHTML = '';
+  response.result.sort((a, b) => {
+    const nameA = a.user.toLowerCase(), nameB = b.user.toLowerCase();
+    if (nameA < nameB) return -1
+    else if (nameA < nameB) return 1;
+    else return 0;
+  });
+  response.result.sort((a, b) => b.score - a.score);
   response.result.forEach((ele) => {
     const list = document.createElement('li');
     list.innerHTML = `<spam>${ele.user}</spam><spam>${ele.score}</spam>`;
